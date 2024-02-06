c0cb0be114b86cf94011c33b172ac8687e9b64c9
Vishn0011 <vishnudhevan11@gmail.com>
2024-02-06 20:05:41 +0800

    New Template

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Heroic Features v5.0.6 (https://startbootstrap.com/template/heroic-features)
+* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
 * Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
