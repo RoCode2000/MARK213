7de944774caeb2ee15963ef87d780e56c95556ec
Vishn0011 <vishnudhevan11@gmmail.com>
2024-02-15 23:58:05 +0800

    Edited product card. Still need update!

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,22 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+// This file contains the JavaScript code to enable card expansion/collapse functionality
+
+document.addEventListener('DOMContentLoaded', function() {
+    var cards = document.querySelectorAll('.card');
+    cards.forEach(function(card) {
+        card.addEventListener('click', function() {
+            var collapse = this.querySelector('.collapse');
+            var isCollapsed = collapse.classList.contains('show');
+            if (isCollapsed) {
+                collapse.classList.remove('show');
+            } else {
+                collapse.classList.add('show');
+            }
+        });
+    });
+});
+
+
